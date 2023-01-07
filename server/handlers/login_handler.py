0662ef96dd6fb37036c0240c40ac7ba0a2ac2f4d
nikkitkit007 <nikkitkit0707@mail.ru>
2023-01-07 17:05:08 +0300

    fix setting cookies

--- server/handlers/login_handler.py
+++ server/handlers/login_handler.py
@@ -5,18 +5,18 @@ import datetime
 from server.services.sso.itmo_id import ItmoId
 
 
-def set_tokens(access_token: str, expires_in: str, refresh_token: str, refresh_expires_in: str):
+def set_tokens(access_token: str, expires_in: int, refresh_token: str, refresh_expires_in: int):
     expires_in = datetime.datetime.utcnow() + datetime.timedelta(seconds=int(expires_in))
     refresh_expires_in = datetime.datetime.utcnow() + datetime.timedelta(seconds=int(refresh_expires_in))
 
-    response = make_response("Here, take some cookie!")
+    response = redirect(location=url_for("index"), code=302)
     response.set_cookie(key="access_token", value=access_token, expires=expires_in, httponly=True)
     response.set_cookie(key="refresh_token", value=refresh_token, expires=refresh_expires_in, httponly=True)
-    response.status_code = status.HTTP_200_OK
+
     return response
 
 
-def login():           # это должен быть redirect_uri !!!!!!!!!!!!!
+def login():  # это должен быть redirect_uri !!!!!!!!!!!!!
     print("login ...")
     code = request.args.get('code')
     print("code", code)
@@ -32,9 +32,7 @@ def login():           # это должен быть redirect_uri !!!!!!!!!!!!!
     expires_in = tokens["expires_in"]
     refresh_expires_in = tokens["refresh_expires_in"]
 
-    set_tokens(access_token=access_token,
-               expires_in=expires_in,
-               refresh_token=refresh_token,
-               refresh_expires_in=refresh_expires_in)
-
-    return redirect(location=url_for("index"), code=302)
+    return set_tokens(access_token=access_token,
+                      expires_in=int(expires_in),
+                      refresh_token=refresh_token,
+                      refresh_expires_in=int(refresh_expires_in))
