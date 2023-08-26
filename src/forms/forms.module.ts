9bc08a3ace166df133a1fd339f1e7286a207af3c
MarikArh <teamtiras@gmail.com>
2023-08-26 15:17:56 +0300

    Guard fix

--- src/forms/forms.module.ts
+++ src/forms/forms.module.ts
@@ -4,12 +4,14 @@ import { FormsController } from './forms.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Form } from './form.model';
 import { UsersModule } from 'users/users.module';
+import { AuthModule } from 'auth/auth.module';
 
 @Module({
     providers: [FormsService],
     controllers: [FormsController],
     imports: [
         forwardRef(() => UsersModule),
+        forwardRef(() => AuthModule),
         SequelizeModule.forFeature([Form])
     ]
 })
