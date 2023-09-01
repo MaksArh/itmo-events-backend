078d9732c4a9d65dc7caf89447653569e62dbe7f
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2023-09-01 18:01:15 +0300

    add imports in events module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Event } from './event.model';
+import { UsersModule } from "users/users.module";
+import { AuthModule } from "auth/auth.module";
 
 @Module({
     controllers: [EventsController],
     providers: [EventsService],
     imports: [
+        forwardRef(() => UsersModule),
+        forwardRef(() => AuthModule),
         SequelizeModule.forFeature([Event])
     ]
 })
