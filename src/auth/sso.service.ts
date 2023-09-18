d396709471366fe03a325ab60b87dc1931d12b4c
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2023-09-18 18:40:16 +0300

    log data for token

--- src/auth/sso.service.ts
+++ src/auth/sso.service.ts
@@ -56,8 +56,8 @@ export class SsoService {
             data.append('client_secret', this.clientSecret);
             data.append('grant_type', 'authorization_code');
             data.append('redirect_uri', this.redirectUri);
-            console.log('[CODE]', code);
             data.append('code', code);
+            console.log('[DATA]', data);
             const response = await axios.post(this.itmoIdTokenUrl, data, {
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
             });
