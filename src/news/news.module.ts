cb1e433fc9228596071dd5f3972ec87c4d33a9c1
MarikArh <teamtiras@gmail.com>
2023-09-21 21:49:16 +0300

    работаю с логами

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { NewsController } from './news.controller';
 import { NewsService } from './news.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { News } from './news.model';
+import { LoggerModule } from 'logger/logger.module';
 
 @Module({
     controllers: [NewsController],
     providers: [NewsService],
     imports: [
+        forwardRef(() => LoggerModule),
         SequelizeModule.forFeature([News])
     ]
 })
